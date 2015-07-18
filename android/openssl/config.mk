b5a7ddfde4c65b2281729df95198451754212cfa
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-07-18 14:28:09 +0900

    update download URL

--- android/openssl/config.mk
+++ android/openssl/config.mk
@@ -1,4 +1,4 @@
-OPENSSL_URL := https://www.openssl.org/source/openssl-1.0.2a.tar.gz
+OPENSSL_URL := https://www.openssl.org/source/openssl-1.0.2d.tar.gz
 OPENSSL_OPTIONS = shared no-ssl2 no-ssl3 no-hw no-engine zlib-dynamic
 
 #### The below codes are automatically set. ####
