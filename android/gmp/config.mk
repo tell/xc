589484164c2d24b9fba559c8f8f499849a74ea02
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-07-28 15:56:58 +0900

    update

--- android/gmp/config.mk
+++ android/gmp/config.mk
@@ -1,9 +1,4 @@
-GMP_URL := https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2
-GMP_DIR = gmp-6.0.0
+include ../../src/gmp/config.mk
 GMP_BUILD = gmp-build
 GMP_HOST = arm-linux-androideabi
-
-#### The below codes are automatically set. ####
-
-GMP_TAR = $(notdir $(GMP_URL))
 GMP_PREFIX = $(PREFIX_DIR)
