91d2037cdd5b0e0ca94aaed3a9f877df14c4de02
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-07-28 21:19:00 +0900

    number of cores is 2 for parallel compilation

--- src/gmp/config.mk
+++ src/gmp/config.mk
@@ -3,4 +3,4 @@ GMP_URL = https://gmplib.org/download/gmp/gmp-6.0.0a.tar.bz2
 GMP_DIR = gmp-6.0.0
 GMP_DIR_PATH = $(GMP_DIR_PREFIX)/$(GMP_DIR)
 GMP_TAR_NAME = $(notdir $(GMP_URL))
-NUM_PARALLEL_BUILD = 8
+NUM_PARALLEL_BUILD = 2
