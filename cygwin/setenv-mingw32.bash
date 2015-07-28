efc4b8f04c48f05eb60c2039f1072b7218f9f4a4
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-07-28 14:38:49 +0900

    add a script to setup environmental variables for MinGW

---
+++ cygwin/setenv-mingw32.bash
@@ -0,0 +1,11 @@
+export AS=x86_64-w64-mingw32-as.exe
+export CC=x86_64-w64-mingw32-gcc.exe
+export CXX=x86_64-w64-mingw32-g++.exe
+export AR=x86_64-w64-mingw32-ar.exe
+export LD=x86_64-w64-mingw32-ld.exe
+export NM=x86_64-w64-mingw32-nm.exe
+export RANLIB=x86_64-w64-mingw32-ranlib.exe
+export OBJDUMP=x86_64-w64-mingw32-objdump.exe
+export STRIP=x86_64-w64-mingw32-strip.exe
+
+export PATH=${PATH}:/usr/x86_64-w64-mingw32/sys-root/mingw/bin
