5a107d480ad0c6c896b70ad9d5331ee1273baa87
Tadanori TERUYA <tadanori.teruya@gmail.com>
2015-12-06 14:17:45 +0900

    update a script to set environmental variables

--- cygwin/setenv-mingw32.bash
+++ cygwin/setenv-mingw32.bash
@@ -1,11 +1,13 @@
-export AS=x86_64-w64-mingw32-as.exe
-export CC=x86_64-w64-mingw32-gcc.exe
-export CXX=x86_64-w64-mingw32-g++.exe
-export AR=x86_64-w64-mingw32-ar.exe
-export LD=x86_64-w64-mingw32-ld.exe
-export NM=x86_64-w64-mingw32-nm.exe
-export RANLIB=x86_64-w64-mingw32-ranlib.exe
-export OBJDUMP=x86_64-w64-mingw32-objdump.exe
-export STRIP=x86_64-w64-mingw32-strip.exe
+ARCH='x86_64-w64-mingw32'
+SYSROOT="/usr/${ARCH}/sys-root"
+export AS="${ARCH}-as.exe"
+export CC="${ARCH}-gcc.exe"
+export CXX="${ARCH}-g++.exe"
+export AR="${ARCH}-ar.exe"
+export LD="${ARCH}-ld.exe"
+export NM="${ARCH}-nm.exe"
+export RANLIB="${ARCH}-ranlib.exe"
+export OBJDUMP="${ARCH}-objdump.exe"
+export STRIP="${ARCH}-strip.exe"
 
-export PATH=$PATH:/usr/x86_64-w64-mingw32/sys-root/mingw/bin
+export PATH="${PATH}:${SYSROOT}/mingw/bin"
